c3f0ba0e547fe8feaa65cb06d2dccea26f29081b
Dmitriy <dashagarov@edu.hse.ru>
2021-10-02 20:12:36 +0300

    Build folder for cmake added

---
+++ build/CMakeFiles/AoCS.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/dmitriy/Рабочий стол/AoCS/Additional_functions/random.cpp" "CMakeFiles/AoCS.dir/Additional_functions/random.cpp.o" "gcc" "CMakeFiles/AoCS.dir/Additional_functions/random.cpp.o.d"
+  "/home/dmitriy/Рабочий стол/AoCS/Data_structures/container.cpp" "CMakeFiles/AoCS.dir/Data_structures/container.cpp.o" "gcc" "CMakeFiles/AoCS.dir/Data_structures/container.cpp.o.d"
+  "/home/dmitriy/Рабочий стол/AoCS/Structures/bus.cpp" "CMakeFiles/AoCS.dir/Structures/bus.cpp.o" "gcc" "CMakeFiles/AoCS.dir/Structures/bus.cpp.o.d"
+  "/home/dmitriy/Рабочий стол/AoCS/Structures/car.cpp" "CMakeFiles/AoCS.dir/Structures/car.cpp.o" "gcc" "CMakeFiles/AoCS.dir/Structures/car.cpp.o.d"
+  "/home/dmitriy/Рабочий стол/AoCS/Structures/truck.cpp" "CMakeFiles/AoCS.dir/Structures/truck.cpp.o" "gcc" "CMakeFiles/AoCS.dir/Structures/truck.cpp.o.d"
+  "/home/dmitriy/Рабочий стол/AoCS/Structures/vehicle.cpp" "CMakeFiles/AoCS.dir/Structures/vehicle.cpp.o" "gcc" "CMakeFiles/AoCS.dir/Structures/vehicle.cpp.o.d"
+  "/home/dmitriy/Рабочий стол/AoCS/main.cpp" "CMakeFiles/AoCS.dir/main.cpp.o" "gcc" "CMakeFiles/AoCS.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/AoCS.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "../bin/AoCS"
+  "../bin/AoCS.pdb"
+  "CMakeFiles/AoCS.dir/Additional_functions/random.cpp.o"
+  "CMakeFiles/AoCS.dir/Additional_functions/random.cpp.o.d"
+  "CMakeFiles/AoCS.dir/Data_structures/container.cpp.o"
+  "CMakeFiles/AoCS.dir/Data_structures/container.cpp.o.d"
+  "CMakeFiles/AoCS.dir/Structures/bus.cpp.o"
+  "CMakeFiles/AoCS.dir/Structures/bus.cpp.o.d"
+  "CMakeFiles/AoCS.dir/Structures/car.cpp.o"
+  "CMakeFiles/AoCS.dir/Structures/car.cpp.o.d"
+  "CMakeFiles/AoCS.dir/Structures/truck.cpp.o"
+  "CMakeFiles/AoCS.dir/Structures/truck.cpp.o.d"
+  "CMakeFiles/AoCS.dir/Structures/vehicle.cpp.o"
+  "CMakeFiles/AoCS.dir/Structures/vehicle.cpp.o.d"
+  "CMakeFiles/AoCS.dir/main.cpp.o"
+  "CMakeFiles/AoCS.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AoCS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
