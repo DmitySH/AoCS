2c3c2a8edfac38ff564987de25484928ba0a68cc
DmiLinux <dashagarov@edu.hse.ru>
2021-10-24 13:26:37 +0300

    test results added

---
+++ Task2/build/CMakeFiles/Task2.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dmitriy/Desktop/AoCS/Task2/Additional_functions/random.cpp" "/home/dmitriy/Desktop/AoCS/Task2/build/CMakeFiles/Task2.dir/Additional_functions/random.cpp.o"
+  "/home/dmitriy/Desktop/AoCS/Task2/Data_structures/container.cpp" "/home/dmitriy/Desktop/AoCS/Task2/build/CMakeFiles/Task2.dir/Data_structures/container.cpp.o"
+  "/home/dmitriy/Desktop/AoCS/Task2/Structures/bus.cpp" "/home/dmitriy/Desktop/AoCS/Task2/build/CMakeFiles/Task2.dir/Structures/bus.cpp.o"
+  "/home/dmitriy/Desktop/AoCS/Task2/Structures/car.cpp" "/home/dmitriy/Desktop/AoCS/Task2/build/CMakeFiles/Task2.dir/Structures/car.cpp.o"
+  "/home/dmitriy/Desktop/AoCS/Task2/Structures/truck.cpp" "/home/dmitriy/Desktop/AoCS/Task2/build/CMakeFiles/Task2.dir/Structures/truck.cpp.o"
+  "/home/dmitriy/Desktop/AoCS/Task2/Structures/vehicle.cpp" "/home/dmitriy/Desktop/AoCS/Task2/build/CMakeFiles/Task2.dir/Structures/vehicle.cpp.o"
+  "/home/dmitriy/Desktop/AoCS/Task2/main.cpp" "/home/dmitriy/Desktop/AoCS/Task2/build/CMakeFiles/Task2.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Task2/build/CMakeFiles/Task2.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "../bin/Task2"
+  "../bin/Task2.pdb"
+  "CMakeFiles/Task2.dir/Additional_functions/random.cpp.o"
+  "CMakeFiles/Task2.dir/Data_structures/container.cpp.o"
+  "CMakeFiles/Task2.dir/Structures/bus.cpp.o"
+  "CMakeFiles/Task2.dir/Structures/car.cpp.o"
+  "CMakeFiles/Task2.dir/Structures/truck.cpp.o"
+  "CMakeFiles/Task2.dir/Structures/vehicle.cpp.o"
+  "CMakeFiles/Task2.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Task2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
